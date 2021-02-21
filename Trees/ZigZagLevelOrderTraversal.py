0d067696d43bf94ecb81f579d9d494e2625b9b00
h1m <himanshu.yadav643@gmail.com>
2021-02-21 07:48:08 +0530

    zigzag level order traversal

--- Trees/ZigZagLevelOrderTraversal.py
+++ Trees/ZigZagLevelOrderTraversal.py
@@ -27,3 +27,35 @@ def zizagtraversal(root):
             ltr = not ltr
             # swapping of stacks
             currentLevel, nextLevel = nextLevel, currentLevel
+
+##################################################################
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+        result = []
+        queue = collections.deque([root])
+        level = False
+        while len(queue) > 0:
+            answer = []
+            length_queue = len(queue)
+            for i in range(length_queue):
+                node = queue.popleft()
+                answer.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            if level:
+                result.append(answer[::-1])
+            else:
+                result.append(answer)
+            level = not level
+        return result
