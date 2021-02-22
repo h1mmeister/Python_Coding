19d3b1375530d999242620e54acf044bdfb50b16
h1m <himanshu.yadav643@gmail.com>
2021-02-22 12:48:27 +0530

    number of islands - dfs approach - graph

---
+++ Graphs/NumberOfIslands.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        number_of_islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    number_of_islands += 1
+        return number_of_islands
+
+    def dfs(self, grid, i, j):
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != "1":
+            return
+        grid[i][j] = "2"
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j + 1)
+        self.dfs(grid, i, j - 1)
